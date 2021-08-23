610cf5b298a46650cab1fb9d55f4a652524676e8
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-08-23 14:33:05 +0300

    done

--- src/Components/categories/Categories.jsx
+++ src/Components/categories/Categories.jsx
@@ -32,32 +32,3 @@ const stateToProps = (state) => ({
 const dispatchToProps = { active };
 
 export default connect(stateToProps, dispatchToProps)(typeNAme);
-// import { Button, Grid } from '@material-ui/core';
-// import { connect } from 'react-redux';
-// import { active } from '../../store/category-reducer';
-
-// const activeCategories = (props) => {
-//     return (
-//         <div>
-//             <Grid >
-//                 {
-//                     props.activeList.categories.map((category) => {
-//                         return (
-//                             <Grid key={category.name}>
-//                                 <Button
-//                                     onClick={() => props.active(category.name)}
-//                                 >{category.display_name}</Button>
-
-//                             </Grid>)
-//                     })
-//                 }
-//             </Grid>
-//         </div>
-//     )
-// }
-
-// const stateToProps = (state) => ({
-//     activeList: state.categories,
-// })
-// const dispatchToProps = { active };
-// export default connect(stateToProps, dispatchToProps)(activeCategories);
